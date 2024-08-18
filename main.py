b308bb5e9aed8f92f3030e894357f0bbfb86c10a
Mahdi <mahdimeyghani02@gmail.com>
2024-08-18 18:04:59 +0330

    Convert module to package

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import invoice
+from invoicing import generate
 
-invoice.generate("invoices", "output", "logo.png",
+generate("invoices", "output", "logo.png",
                  "product_id", "product_name", "amount_purchased",
                  "price_per_unit", "total_price")
