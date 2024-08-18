60bf9807ee84932fd5dde00d36185069a69b18ab
Mahdi <mahdimeyghani02@gmail.com>
2024-08-18 18:05:42 +0330

    Delete testing files

--- main.py
+++
@@ -1,5 +0,0 @@
-from invoicing import generate
-
-generate("invoices", "output", "logo.png",
-                 "product_id", "product_name", "amount_purchased",
-                 "price_per_unit", "total_price")
