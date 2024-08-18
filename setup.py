0b23e0cbc8a81acba8c588fcc9fcd97214f38272
Mahdi <mahdimeyghani02@gmail.com>
2024-08-18 19:47:33 +0330

    Change Intended audience

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers :: Companies',  # Who is the audience for your library?
+        'Intended Audience :: Developers',  # Who is the audience for your library?
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',  # Type a license again
         'Programming Language :: Python :: 3.8',  # Python versions that your library supports
