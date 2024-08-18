47f7f8e86c92ba7347dde19ac6ef32284b7bd54f
Mahdi <mahdimeyghani02@gmail.com>
2024-08-18 17:50:30 +0330

    create PDFs folder automatically if it isn't existed

--- invoice.py
+++ invoice.py
@@ -75,5 +75,7 @@ def generate(invoices_path, pdfs_path, logo_image_path, product_id_col, product_
 
         pdf.image(logo_image_path, w=18, h=18)
 
-        os.makedirs(pdfs_path, exist_ok=True)
+        if not os.path.exist(pdfs_path):
+            os.makedirs(pdfs_path)
+
         pdf.output(f"{pdfs_path}/{filename}.pdf")
